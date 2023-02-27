16ca14c971b129787f9944001ff00bfc8802d39a
tomray6971 <tomray6971@gmail.com>
2023-02-27 21:55:14 +0800

    利用yarn安裝bootstrap套件

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import './bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 function App() {
